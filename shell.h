a9caeb5594f2fc0bcfb03c9fa08b563cacd2ec62
Snr1472s <obinomengodswillsnr1472@gmail.com>
2023-09-12 17:03:56 +0100

    shell_task

--- shell.h
+++ shell.h
@@ -11,7 +11,6 @@
 #include <limits.h>
 #include <signal.h>
 
-
 /**
  * struct variables - variables
  * @av: command line arguments
@@ -34,6 +33,7 @@ typedef struct variables
 	char **commands;
 } vars_t;
 
+
 /**
  * struct builtin - struct for the builtin functions
  * @name: name of builtin command
@@ -46,15 +46,6 @@ typedef struct builtin
 	void (*f)(vars_t *);
 } builtin_t;
 
-char **make_env(char **env);
-void free_env(char **env);
-
-ssize_t _puts(char *str);
-char *_strdup(char *strtodup);
-int _strcmpr(char *strcmp1, char *strcmp2);
-char *_strcat(char *strc1, char *strc2);
-unsigned int _strlen(char *str);
-
 char **tokenize(char *buffer, char *delimiter);
 char **_realloc(char **ptr, size_t *size);
 char *new_strtok(char *str, const char *delim);
@@ -65,19 +56,28 @@ void _env(vars_t *vars);
 void new_setenv(vars_t *vars);
 void new_unsetenv(vars_t *vars);
 
-void add_key(vars_t *vars);
-char **find_key(char **env, char *key);
-char *add_value(char *key, char *value);
-int _atoi(char *str);
-
 void check_for_path(vars_t *vars);
 int path_execute(char *command, vars_t *vars);
 char *find_path(char **env);
 int execute_cwd(vars_t *vars);
 int check_for_dir(char *str);
 
+void add_key(vars_t *vars);
+char **find_key(char **env, char *key);
+char *add_value(char *key, char *value);
+int _atoi(char *str);
+
 void print_error(vars_t *vars, char *msg);
-void _puts2(char *str);
+void _puts_2(char *str);
 char *_uitoa(unsigned int count);
+
+ssize_t _puts(char *str);
+char *_strdup(char *strtodup);
+int _strcmpr(char *strcmp1, char *strcmp2);
+char *_strcat(char *strc1, char *strc2);
+unsigned int _strlen(char *str);
+
+char **make_env(char **env);
+void free_env(char **env);
 
 #endif
